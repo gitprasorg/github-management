1aac89fec29a0f4b41688f92bddf70a7db0e0caf
awesomepras <prasana@alienfromopondy.com>
2025-03-02 22:42:25 -0500

    removed github_token variable

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "GITHUB_TOKEN" {
-  type        = string
-  description = "GitHub Personal Access Token for authentication"
-  sensitive   = true
-}
+#variable "GITHUB_TOKEN" {
+#  type        = string
+#  description = "GitHub Personal Access Token for authentication"
+#  sensitive   = true
+#}
 variable "GITHUB_ORGANIZATION" {
   type  = string
   description = "Github organization"
