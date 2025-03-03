e2cce87973ec1ad7aa443814004c98de2dd419a8
awesomepras <prasana@alienfromopondy.com>
2025-03-02 21:46:58 -0500

    updated terraform settings

--- provider.tf
+++ provider.tf
@@ -1,15 +1,28 @@
 # # Configure the GitHub Provider
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 6.0"
-#     }
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+# provider "github" {
+#   owner = var.github_organization
+#   app_auth {
+#     id              = var.app_id              # or `GITHUB_APP_ID`
+#     installation_id = var.app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
+#     pem_file        = var.app_pem_file        # or `GITHUB_APP_PEM_FILE`
 #   }
 # }
-
 provider "github" {
-  #version      = "~> 2.2" # legacy
   owner = var.github_organization
-  # Export the GITHUB_TOKEN as an environment variable
+  app_auth {} # When using `GITHUB_APP_XXX` environment variables
 }
+
+#token: When not provided or made available via the GITHUB_TOKEN environment variable, the provider can only access resources available anonymously.
+# https://registry.terraform.io/providers/integrations/github/latest/docs
+
+# REquired env variables
+# GITHUB_APP_PEM_FILE, GITHUB_ORGANIZATION, GITHUB_APP_ID, GITHUB_APP_INSTALLATION_ID 
+# needed to use \\n within the GITHUB_APP_PEM_FILE environment instead of \n to get it working.
---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+   required_version = ">= 1.10.0, < 1.12.0"
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.58"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">= 6.0"  # Pin the GitHub provider version
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = ">= 4.3.0, < 5.0.0"
+    }
+  }
+}
