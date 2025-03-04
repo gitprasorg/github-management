a7e03706848ffa341083768ad0c89a334f7716a8
awesomepras <prasana@alienfromopondy.com>
2025-03-03 23:00:00 -0500

    commented out backend

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
-# Add the terraform cloud backend for running locally
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = var.GITHUB_ORGANIZATION
+# # Add the terraform cloud backend for running locally
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = var.GITHUB_ORGANIZATION
 
-    workspaces {
-      name    = var.VCS_WORKSPACES
-      prefix  = "github-vcs-00"
-    }
-  }
-}
+#     workspaces {
+#       name    = var.VCS_WORKSPACES
+#       prefix  = "github-vcs-00"
+#     }
+#   }
+# }
