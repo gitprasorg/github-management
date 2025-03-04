79f77b95cba90f940aa86b5fe8a0f6739b6f8ffc
awesomepras <prasana@alienfromopondy.com>
2025-03-03 22:29:09 -0500

    fixed provider and variables.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
 # # Configure the GitHub Provider
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
 # provider "github" {
 #   owner = var.github_organization
 #   app_auth {
--- variables.tf
+++ variables.tf
@@ -1,11 +1,15 @@
-#variable "GITHUB_TOKEN" {
-#  type        = string
-#  description = "GitHub Personal Access Token for authentication"
-#  sensitive   = true
-#}
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub Personal Access Token for authentication"
+  sensitive   = true
+}
 variable "GITHUB_ORGANIZATION" {
   type  = string
   description = "Github organization"
   sensitive = false
-  
+
+variable "VCS_WORKSPACES" {
+  type  = string
+  description = "Workflow for current VCS"
+  sensitive = false
 }
